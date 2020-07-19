eef768a5070638220d442b22e2d9136a6a48fddd
João <joaopfernandesc@gmail.com>
2020-07-19 18:16:26 -0300

    Get real available mentors

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -3,9 +3,16 @@ class MentorsController < ApplicationController
     def index
         begin
             raise HackARocketExceptions::BadParameters if params[:start_timestamp].nil? || params[:path_id].nil?
+            Time.zone = "America/Sao_Paulo"
+            time_at = Time.zone.at(params[:start_time])
+            start_time = time_at.hour*3600
+            weekday = time_at.wday
 
+            id_raw_available_users = RegularTime.where(start_time: start_time, weekday: weekday).pluck(:user_id)
             busy_users = Appointment.where(start_timestamp: start_timestamp).select(:mentor_id).distinct.pluck(:mentor_id)
-            id_available_users = UserPath.where(path_id: params[:path_id]).where.not(user_id: busy_users).pluck(:user_id)
+
+            path_users = UserPath.where(path_id: params[:path_id]).where.not(user_id: busy_users).pluck(:user_id)
+            id_available_users = path_users - (id_raw_available_users - busy_users)
 
             available_users = User.where(id: id_available_users).order("average_rating ASC").map(&:json_object)
             
