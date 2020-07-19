e6028c685e4dc32836dd119b06d8a8c424cbe952
João <joaopfernandesc@gmail.com>
2020-07-19 12:31:29 -0300

    Regular time

--- app/controllers/regular_times_controller.rb
+++ app/controllers/regular_times_controller.rb
@@ -1,11 +1,53 @@
 class RegularTimesController < ApplicationController
+    before_action :authorized
     def create
-        
+        begin
+            raise HackARocketExceptions::BadParameters if params[:weekday].nil? || params[:start_time].nil? || params[:end_time].nil?
+            rt = RegularTime.new(times_create_params)
+            rt[:user_id] = @user[:id]
+            rt.save!
+
+            render json: rt.json_object, status: 201
+        rescue HackARocketExceptions::BadParameters
+            render json: {error: "Bad parameters"}, status: 400
+        rescue => e
+            Rails.logger.error e.message
+            Rails.logger.error e.backtrace.join("\n")
+            render status: 500
+        end
     end
     def index
-        
+        begin
+            user_id = @user[:id]
+
+            rts = RegularTime.where(user_id: user_id)
+
+            rts = rts.map { |x| x.json_object }
+
+            render json: rts, status: 200
+        rescue => e
+            Rails.logger.error e.message
+            Rails.logger.error e.backtrace.join("\n")
+            render status: 500
+        end
     end    
     def destroy
-        
+        begin
+            rt = RegularTime.find(params[:id])
+            rt.destroy
+
+            render status: 204
+        rescue ActiveRecord::RecordNotFound
+            render json: {error: "Not found"}, status: 404
+        rescue => e
+            Rails.logger.error e.message
+            Rails.logger.error e.backtrace.join("\n")
+            render status: 500
+        end
     end
+
+    def times_create_params
+        params.permit(:weekday, :start_time, :end_time)
+    end
+    
 end
--- app/models/regular_time.rb
+++ app/models/regular_time.rb
@@ -1,3 +1,14 @@
 class RegularTime < ApplicationRecord
     acts_as_paranoid
+
+    def json_object
+        return {
+            id: self.id,
+            user_id: self.user_id,
+            start_time: self.start_time,
+            end_time: self.end_time,
+            weekday: self.weekday
+        }
+    end
+    
 end
